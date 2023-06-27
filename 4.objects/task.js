7d0002a65db461b1591ee1bca552a4cea2c113be
AlexandrKov <mulah1988@mail.ru>
2023-06-27 07:56:15 +0300

    new commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,38 @@
-function Student(name, gender, age) {
-  
-}
-
-Student.prototype.setSubject = function (subjectName) {
-  
-}
-
-Student.prototype.addMarks = function (...marks) {
-  
-}
-
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
+    function Student(name, gender, age) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.marks = [];
+    }
+    
+    
+    Student.prototype.setSubject = function (subjectName) {
+      this.subject = subjectName;
+    }
+    
+    Student.prototype.addMarks = function (...marks) {
+      if (this.hasOwnProperty('marks') && this.marks !== []) {
+        this.marks.push(...marks);
+      } else {
+        return 0; 
+      }
+    }
+    
+    Student.prototype.getAverage = function () {
+      if (this.hasOwnProperty('marks') === false || this.marks.length === 0) {
+        this.average = 0;
+        return this.average;
+      } else {
+        let sum = 0;
+        let count = this.marks.length;
+        sum = this.marks.reduce((acc, mark) => acc + mark, 0)
+        this.average = sum / count;
+        return sum / count;
+      }
+    }
+    
+    Student.prototype.exclude = function (reason) {
+        this.exclude = reason;
+        delete this.marks;
+        delete this.subject;
+      }
